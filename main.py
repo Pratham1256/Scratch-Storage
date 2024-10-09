2c6475b33c820524f6f93c5fcf92f98a98815291
Pratham1256 <83955212+Pratham1256@users.noreply.github.com>
2024-10-09 11:24:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import app
 import os
 
 from scratch2py import Scratch2Py
-s2py = Scratch2Py('pratham_test', os.environ.get('PASS'))
+s2py = Scratch2Py('JethreeH', os.environ.get('PASS'))
 
 app.keep_alive()
 
