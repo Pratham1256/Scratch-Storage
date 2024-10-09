2ddc122b4f0e0ed5fa6d581c85695bb62443e7eb
Pratham1256 <83955212+Pratham1256@users.noreply.github.com>
2024-10-09 12:05:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,27 @@ s2py = Scratch2Py('JethreeH', os.environ.get('PASS'))
 
 app.keep_alive()
 
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+def readCloudVar( projectId , key ):
+    cloudProject = s2py.scratchConnect(projectId)
+    value = cloudProject.readCloudVar(key)
+    return value
+
+def setCloudVar( projectId , key , value):
+    cloudProject = s2py.scratchConnect(projectId)
+    try :
+        cloudProject.setCloudVar(key, value)
+        return True
+    except:
+        return False
+
+
 while True:
+  for project in data:
+    if not(readCloudVar(project, 'transferring') == 0):
+      # Parse the data
+      pass
+      
   pass
