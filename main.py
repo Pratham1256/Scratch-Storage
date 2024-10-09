5c9dd3e496befb2515826108ad594b8d5ccd4e50
Pratham1256 <83955212+Pratham1256@users.noreply.github.com>
2024-10-09 18:31:05 +0530

    Update main

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ with open("data.json", "r") as file:
 cloudProject = None
 
 
-def readCloudVar(projectId, key, connect=False):
+def readCloudVar(projectId, key, connect=True):
     if connect:
         cloudProject = s2py.scratchConnect(projectId)
     value = cloudProject.readCloudVar(key)
     return value
 
 
-def setCloudVar(projectId, key, value, connect=False):
+def setCloudVar(projectId, key, value, connect=True):
     if connect:
         cloudProject = s2py.scratchConnect(projectId)
     try:
@@ -37,24 +37,24 @@ while True:
     old_data = data
     for project in data:
 
-        transferring = int(readCloudVar(project, "transferring", True))
+        transferring = int(readCloudVar(project, "transferring"))
         if not (transferring == 0):
             # Parse the data
             if transferring == 1:
-                en_key = readCloudVar(project, "key")
-                en_val = readCloudVar(project, "value")
+                en_key = readCloudVar(project, "key", False)
+                en_val = readCloudVar(project, "value", False)
                 key = helpers.decode(en_key)
                 val = helpers.decode(en_val)
                 data[project].append({key: val})
-                setCloudVar("transferring", "0")
+                setCloudVar("transferring", "0", False)
             if transferring == 2:
-                en_key = readCloudVar(project, "key")
+                en_key = readCloudVar(project, "key", False)
                 key = helpers.decode(en_key)
                 val = data[project][key]
                 en_val = helpers.encode(val)
-                setCloudVar("value", en_val)
-                setCloudVar("transferring", "3")
-                while int(readCloudVar(project, "key")) == 3:
+                setCloudVar("value", en_val, False)
+                setCloudVar("transferring", "3", False)
+                while int(readCloudVar(project, "key", False)) == 3:
                     time.sleep(0.1)
 
     if not (data == old_data):
