723213377d5667e9ea1aef3378b5841286fdc493
Pratham1256 <83955212+Pratham1256@users.noreply.github.com>
2024-10-09 15:43:30 +0530

    Added checking of change in request

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import app
 import os
+import helpers
+import time
 
 from scratch2py import Scratch2Py
 s2py = Scratch2Py('JethreeH', os.environ.get('PASS'))
@@ -8,14 +10,17 @@ app.keep_alive()
 
 with open('data.json', 'r') as file:
     data = json.load(file)
+cloudProject = None
 
-def readCloudVar( projectId , key ):
-    cloudProject = s2py.scratchConnect(projectId)
+def readCloudVar( projectId , key , connect=False):
+    if connect:
+        cloudProject = s2py.scratchConnect(projectId)
     value = cloudProject.readCloudVar(key)
     return value
 
-def setCloudVar( projectId , key , value):
-    cloudProject = s2py.scratchConnect(projectId)
+def setCloudVar( projectId , key , value, connect=False):
+    if connect:
+        cloudProject = s2py.scratchConnect(projectId)
     try :
         cloudProject.setCloudVar(key, value)
         return True
@@ -24,9 +29,32 @@ def setCloudVar( projectId , key , value):
 
 
 while True:
+  old_data = data
   for project in data:
-    if not(readCloudVar(project, 'transferring') == 0):
-      # Parse the data
-      pass
       
-  pass
+      transferring = int(readCloudVar(project, 'transferring', True))
+    if not(transferring == 0):
+      # Parse the data
+      if transferring == 1:
+          en_key = readCloudVar(project, 'key')
+          en_val = readCloudVar(project, 'value')
+          key = helpers.decode(en_key)
+          val = helpers.decode(en_val)
+          data[project].append({key: val})
+          setCloudVar('transferring', '0')
+      if transferring == 2:
+          en_key = readCloudVar(project, 'key')
+          key = helpers.decode(en_key)
+          val = data[project][key]
+          en_val = helpers.encode(val)
+          setCloudVar('value', en_val)
+          setCloudVar('transferring', '3')
+          while int(readCloudVar(project, 'key')) == 3:
+              time.sleep(0.1)
+
+  if not(data == old_data):
+      with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4) 
+        
+      
+
