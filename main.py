84130d8b6c7428ed361202006f4320a283e85f42
Pratham1256 <83955212+Pratham1256@users.noreply.github.com>
2024-10-09 15:53:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ import helpers
 import time
 
 from scratch2py import Scratch2Py
-s2py = Scratch2Py('JethreeH', os.environ.get('PASS'))
+
+s2py = Scratch2Py("JethreeH", os.environ.get("PASS"))
 
 app.keep_alive()
 
-with open('data.json', 'r') as file:
+with open("data.json", "r") as file:
     data = json.load(file)
-cloudProject = None
+    cloudProject = None
 
-def readCloudVar( projectId , key , connect=False):
+
+def readCloudVar(projectId, key, connect=False):
     if connect:
         cloudProject = s2py.scratchConnect(projectId)
     value = cloudProject.readCloudVar(key)
     return value
 
-def setCloudVar( projectId , key , value, connect=False):
+
+def setCloudVar(projectId, key, value, connect=False):
     if connect:
         cloudProject = s2py.scratchConnect(projectId)
-    try :
+    try:
         cloudProject.setCloudVar(key, value)
         return True
     except:
@@ -29,32 +32,29 @@ def setCloudVar( projectId , key , value, connect=False):
 
 
 while True:
-  old_data = data
-  for project in data:
-      
-      transferring = int(readCloudVar(project, 'transferring', True))
-    if not(transferring == 0):
-      # Parse the data
-      if transferring == 1:
-          en_key = readCloudVar(project, 'key')
-          en_val = readCloudVar(project, 'value')
-          key = helpers.decode(en_key)
-          val = helpers.decode(en_val)
-          data[project].append({key: val})
-          setCloudVar('transferring', '0')
-      if transferring == 2:
-          en_key = readCloudVar(project, 'key')
-          key = helpers.decode(en_key)
-          val = data[project][key]
-          en_val = helpers.encode(val)
-          setCloudVar('value', en_val)
-          setCloudVar('transferring', '3')
-          while int(readCloudVar(project, 'key')) == 3:
-              time.sleep(0.1)
+    old_data = data
+    for project in data:
 
-  if not(data == old_data):
-      with open('data.json', 'w') as file:
-        json.dump(data, file, indent=4) 
-        
-      
+        transferring = int(readCloudVar(project, "transferring", True))
+        if not (transferring == 0):
+            # Parse the data
+            if transferring == 1:
+                en_key = readCloudVar(project, "key")
+                en_val = readCloudVar(project, "value")
+                key = helpers.decode(en_key)
+                val = helpers.decode(en_val)
+                data[project].append({key: val})
+                setCloudVar("transferring", "0")
+            if transferring == 2:
+                en_key = readCloudVar(project, "key")
+                key = helpers.decode(en_key)
+                val = data[project][key]
+                en_val = helpers.encode(val)
+                setCloudVar("value", en_val)
+                setCloudVar("transferring", "3")
+                while int(readCloudVar(project, "key")) == 3:
+                    time.sleep(0.1)
 
+    if not (data == old_data):
+        with open("data.json", "w") as file:
+            json.dump(data, file, indent=4)
